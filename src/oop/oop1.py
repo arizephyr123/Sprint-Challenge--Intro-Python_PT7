40014a6b2d75b67ca40693b6906a41aa5fee2220
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-05-15 19:51:57 -0700

    oop1.py completed, pass test

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,39 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# base class => object
+
+
+class Vehicle:
+    pass
+
+# base class => Vehicle
+
+
+class FlightVehicle(Vehicle):
+    pass
+
+
+class GroundVehicle(Vehicle):
+    pass
+
+# base class => FlightVehicle
+
+
+class Starship(FlightVehicle):
+    pass
+
+
+class Airplane(FlightVehicle):
+    pass
+
+# base class => GroundVehicle
+
+
+class Car(GroundVehicle):
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    pass
